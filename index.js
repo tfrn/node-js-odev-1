04a5e183be881e7822c5df83aceb6fb1f4da38e6
Tolga Fermanci <tfrn@live.fr>
2022-12-19 11:56:37 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-# node-js-odev-1
-patika.dev node.js dersi 1. ödevi 
-###### TERMİNALDEN GİRİLEN DEĞERLE DAİRENİN ALANI HESAPLAMA;
+// Node.js dersi 1. ödevi, -- Dairenin Alanı
 
-```js
 // dairenin alanı: pixr2
 let pi = 3.14;
 let yaricap = parseInt(process.argv.slice(2)); // process.argv.slice(2) ile terminalden değer aldık. node index.js 12 gibi.
 let hesapla= pi*Math.pow(yaricap,2); // Math.pow ile karesini aldık. 
 console.log(hesapla); // sonucu döndürdük.
-```
